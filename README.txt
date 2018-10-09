f61dc50e8de233e4e6d9b8c106df35929e8a7100
Unknown <up201603811@fe.up.pt>
2018-10-09 13:26:18 +0100

    Full sphere

--- README.txt
+++
@@ -1,8 +0,0 @@
-LAIG Demo Parser
-
-Base code and demo scene provided by:
-- Rui Pedro Peixoto Cardoso - up201305469@fe.up.pt
-- Diogo da Silva Amaral - up201306082@fe.up.pt
-
-Adapted by:
-- Rui Rodrigues - rui.rodrigues@fe.up.pt
--- primitives/MySphere.js
+++ primitives/MySphere.js
@@ -1,4 +1,4 @@
-class MySphere extends CGFobject
+class MySemiSphere extends CGFobject
 {
 	constructor(scene, slices, stacks, radius, minS = 0, maxS = 1, minT = 0, maxT = 1)
 	{
@@ -34,19 +34,19 @@ class MySphere extends CGFobject
         		// y = r cosθ
         		// z = r sinθ sinα
 
-				this.vertices.push( this.radius*Math.cos(this.alpha*j)*Math.sin(this.theta*i), 
+				this.vertices.push( this.radius*Math.cos(this.alpha*j)*Math.sin(this.theta*i),
 					this.radius*Math.sin(this.alpha*j)*Math.sin(this.theta*i), this.radius*Math.cos(this.theta*i));
 
-				this.normals.push( this.radius*Math.cos(this.alpha*j)*Math.sin(this.theta*i), 
+				this.normals.push( this.radius*Math.cos(this.alpha*j)*Math.sin(this.theta*i),
 					this.radius*Math.sin(this.alpha*j)*Math.sin(this.theta*i), this.radius*Math.cos(this.theta*i));
 
-				this.texCoords.push(this.minS + i * (this.maxS - this.minS) / this.slices, 
+				this.texCoords.push(this.minS + i * (this.maxS - this.minS) / this.slices,
 					this.minT + j * (this.maxT - this.minT) / this.stacks);
 		 	}
    		}
 
 		 // Draw triangles
-		 
+
 	   	for(j = 0; j < this.stacks; j++)
 		{
 	    	for(i = 0; i < this.slices; i++)
@@ -54,8 +54,8 @@ class MySphere extends CGFobject
 	       		//First
 	       		this.indices.push(this.slices*j+i);
 	       		this.indices.push(this.slices*j+i+1-(i < this.slices-1 ? 0 : this.slices));
-				this.indices.push(this.slices*(j+1)+i+1-(i < this.slices-1 ? 0 : this.slices));
-				   
+						this.indices.push(this.slices*(j+1)+i+1-(i < this.slices-1 ? 0 : this.slices));
+
 	       		//Second
 	       		this.indices.push(this.slices*(j+1)+i+1-(i < this.slices-1 ? 0 : this.slices));
 	       		this.indices.push(this.slices*(j+1)+i);
@@ -67,3 +67,30 @@ class MySphere extends CGFobject
  	this.initGLBuffers();
  	};
 };
+
+class MySphere extends CGFobject
+{
+	constructor(scene, slices, stacks, radius, minS = 0, maxS = 1, minT = 0, maxT = 1)
+	{
+        super(scene);
+        this.slices = slices; //longitude slices
+        this.stacks = stacks; //Latitude stacks
+        this.radius = radius;
+				this.minS = minS;
+        this.maxS = maxS;
+        this.minT = minT;
+        this.maxT = maxT;
+
+				this.semiSphere = new MySemiSphere(this.scene, slices, stacks, radius, minS, maxS, minT, maxT);
+
+	};
+
+	display(){
+		this.scene.pushMatrix();
+		this.semiSphere.display();
+		this.scene.rotate(Math.PI, 0, 1, 0);
+		this.scene.translate(0, 0, -0.5);
+		this.semiSphere.display();
+		this.scene.popMatrix();
+	}
+}
