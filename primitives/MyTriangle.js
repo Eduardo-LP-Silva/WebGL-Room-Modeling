e1759ab8b0cbafd45a383816bc84cc5fdaa96f4e
Unknown <up201603811@fe.up.pt>
2018-10-16 15:33:40 +0100

    merge

--- primitives/MyTriangle.js
+++ primitives/MyTriangle.js
@@ -25,9 +25,8 @@ class MyTriangle extends CGFobject
 			2, 1, 0
 		];
 
-		this.primitiveType=this.scene.gl.TRIANGLES;
 
-		const vecAB = [this.vCoords[1][0] - this.vCoords[0][0], this.vCoords[1][1] - this.vCoords[0][1], this.vCoords[1][2] - this.vCoords[0][2]];
+		  const vecAB = [this.vCoords[1][0] - this.vCoords[0][0], this.vCoords[1][1] - this.vCoords[0][1], this.vCoords[1][2] - this.vCoords[0][2]];
     	const vecAC = [this.vCoords[2][0] - this.vCoords[0][0], this.vCoords[2][1] - this.vCoords[0][1], this.vCoords[2][2] - this.vCoords[0][2]];
     	const vecBC = [this.vCoords[2][0] - this.vCoords[1][0], this.vCoords[2][1] - this.vCoords[1][1], this.vCoords[2][2] - this.vCoords[1][2]];
 
@@ -37,17 +36,32 @@ class MyTriangle extends CGFobject
         vecAB[0] * vecAC[1] - vecAB[1] * vecAC[0]
     ];
 
-		this.normals=[normalVector[0], normalVector[1], normalVector[2], normalVector[0], normalVector[1], 
+		this.normals=[normalVector[0], normalVector[1], normalVector[2], normalVector[0], normalVector[1],
 			normalVector[2], normalVector[0], normalVector[1], normalVector[2]];
 
+
+		//based on the file given
+		var b = Math.sqrt(vecAB[0]**2 + vecAB[1]**2 + vecAB[2]**2);
+		var a = Math.sqrt(vecAC[0]**2 + vecAC[1]**2 + vecAC[2]**2);
+		var c = Math.sqrt(vecBC[0]**2 + vecBC[1]**2 + vecBC[2]**2);
+
+		var cosBeta = (a**2 - b**2 + c**2)/(2*c*a);
+
+		var sinBeta = Math.sqrt(a**2-(a*cosBeta)*(a*cosBeta))/a;
+
 		this.texCoords = [
-			this.minS, this.maxT, // 0,1
-			this.maxS, this.maxT, // 1,1,
-			this.minS, this.minT, // 0,0,
-			this.maxS, this.minT // 1,0
+			c-a*cosBeta, 1-a*sinBeta,
+			0, 1,
+			c, 1
+
 		];
 
+
+
+
+		this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
+
 	};
 
 	update(maxS, maxT)
